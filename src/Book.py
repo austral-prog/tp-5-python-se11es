a11acb2453f1cf54a90fb79310cad02b734f83f4
se11es <162724132+se11es@users.noreply.github.com>
2024-09-12 18:51:31 -0300

    Update Book.py

--- src/Book.py
+++ src/Book.py
@@ -1,40 +1,38 @@
-# from typing import Self
-
+from typing import Optional
 
 class Book:
-    def __init__(self, isbn, title, author, available=True, checkout_num=0):
+    def __init__(self, isbn: str, title: str, author: str, available: bool = True, checkout_num: int = 0):
         self.__isbn = isbn
         self.__title = title
         self.__author = author
         self.__available = available
         self.__checkout_num = checkout_num
 
-    # Getters
-    def get_isbn(self):
+    def get_isbn(self) -> str:
         return self.__isbn
 
-    def get_title(self):
+    def get_title(self) -> str:
         return self.__title
 
-    def get_author(self):
+    def get_author(self) -> str:
         return self.__author
 
-    def is_available(self):
+    def is_available(self) -> bool:
         return self.__available
 
-    def get_checkout_num(self):
+    def get_checkout_num(self) -> int:
         return self.__checkout_num
 
-    # Setters
-    def set_available(self, available):
-        pass
+    def set_available(self, available: bool) -> None:
+        self.__available = available
 
-    def increment_checkout_num(self):
-        pass
+    def increment_checkout_num(self) -> None:
+        self.__checkout_num += 1
 
-    # Utils
     def __str__(self) -> str:
-        pass
+        return f"ISBN: {self.__isbn}, Title: {self.__title}, Author: {self.__author}, Available: {self.__available}"
 
-    def __eq__(self, other):
-        pass
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Book):
+            return False
+        return self.__isbn == other.__isbn
